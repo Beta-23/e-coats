19f0de38351d7b07ed270b539c87437cfda54404
Beta-23 <alfred.dominguez@gmail.com>
2019-12-13 11:59:13 -0800

    Build directory, menu item components and sass style

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,41 +1,10 @@
 import React from 'react';
+import Directory from '../../components/directory/directory.component';
 import './homepage.styles.scss';
 
 const HomePage = () => (
     <div className='homepage'>
-      <h1>Welcome to E-Coats!</h1>
-      <div className='directory-menu'>
-        <div className='menu-item'>
-          <div className='content'>
-            <div className='title'>BOYS COATS</div>
-            <span className='subtitle'>SHOP NOW</span>
-          </div>
-        </div>
-        <div className='menu-item'>
-          <div className='content'>
-            <div className='title'>JACKETS</div>
-            <span className='subtitle'>SHOP NOW</span>
-          </div>
-        </div>
-        <div className='menu-item'>
-          <div className='content'>
-            <div className='title'>GIRLS COATS</div>
-            <span className='subtitle'>SHOP NOW</span>
-          </div>
-        </div>
-        <div className='menu-item'>
-          <div className='content'>
-            <div className='title'>WOMENS COATS</div>
-            <span className='subtitle'>SHOP NOW</span>
-          </div>
-        </div>
-        <div className='menu-item'>
-          <div className='content'>
-            <div className='title'>MENS COATS</div>
-            <span className='subtitle'>SHOP NOW</span>
-          </div>
-        </div>
-      </div>
+      <Directory />
     </div>
   );
 
