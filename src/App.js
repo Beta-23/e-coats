48cdd24f22168c97ef4d5764f6bf8913f55e8a32
Beta-23 <alfred.dominguez@gmail.com>
2019-12-22 18:54:16 -0800

    Implement the Auth firebase library to manage users when state changes

--- src/App.js
+++ src/App.js
@@ -5,18 +5,43 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
+import { auth } from './firebase/firebase.utils';
 
 // Switch renders the first matched path
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path='/' component= {HomePage} />
-        <Route path='/shop' component= {ShopPage} />
-        <Route path='/signin' component= {SignInAndSignUpPage} />
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      currentUser: null
+    }
+  }
+  // Preventing JS memory leaks
+  unsubscribeFromAuth = null
+
+
+  // Messaging between app and firebase when any changes happens
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user });
+      console.log(user);
+    });
+  }
+
+  // LifeCycle method Close the subscrition
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+  render() {
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path='/' component= {HomePage} />
+          <Route path='/shop' component= {ShopPage} />
+          <Route path='/signin' component= {SignInAndSignUpPage} />
+        </Switch>
+      </div>
+    );
+  }
 }
 export default App;
