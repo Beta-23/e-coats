59dd3c0d9e2dcb31989f38c5498cd555b6b41169
Beta-23 <alfred.dominguez@gmail.com>
2019-12-30 22:02:33 -0800

    Add styles to the cart-icon component and integrated it into the header component

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -8,7 +8,6 @@ const CartIcon = () => (
     <div className='cart-icon'>
         <ShoppingIcon className='shopping-icon' />
         <span className='item-count'>0</span>
-    
     </div>
 );
 export default CartIcon;
