f61562e06d9fe6ef79e734cc89ac66784e3263c9
Beta-23 <alfred.dominguez@gmail.com>
2019-12-30 22:28:17 -0800

    Update checkout button style

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -7,7 +7,7 @@ import './cart-dropdown.styles.scss';
 const CartDropdown = () => (
     <div className='cart-dropdown'>
         <div className='cart-items' />
-        <CustomButton>CHECK OUT NOW</CustomButton>
+        <CustomButton>CHECK OUT</CustomButton>
     </div>
 );
 
