fd0e95889a5a9823ff29020e13b01bb197bed8ba
Beta-23 <alfred.dominguez@gmail.com>
2020-01-04 22:49:26 -0800

    Build the add multiple items to cart utility

---
+++ src/redux/cart/cart.utils.js
@@ -0,0 +1,18 @@
+// Checks for duplicate cartItems and return a new version of state
+export const addItemToCart = (cartItems, cartItemToAdd) => {
+    const existingCartItem = cartItems.find(
+      cartItem => cartItem.id === cartItemToAdd.id
+    );
+  
+    if (existingCartItem) {
+      return cartItems.map(cartItem =>
+        cartItem.id === cartItemToAdd.id
+          ? { ...cartItem, quantity: cartItem.quantity + 1 }
+          : cartItem
+      )
+    }
+  
+    return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+  };
+  
+
