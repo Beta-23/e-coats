2c61e61640f27b3fc5da24d65caa118f2c981625
Beta-23 <alfred.dominguez@gmail.com>
2020-01-15 22:20:40 -0800

    Build the shop selectors and connect to the shop component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
-
+import { connect } from 'react-redux';
 import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+import { selectCollections } from '../../redux/shop/shop.selectors';
+import { createStructuredSelector } from 'reselect';
 
-const ShopPage = ({ collections, }) =>(
+const ShopPage = ({ collections, }) => (
             <div className='shop-page'>
             {
                 collections.map(({id, ...otherCollectionProps }) => (
@@ -10,5 +12,9 @@ const ShopPage = ({ collections, }) =>(
                 ))
             }
         </div>);
-
-export default ShopPage;
+        
+const mapStateToProps = createStructuredSelector({ 
+    collections: selectCollections
+  });
+  
+  export default connect(mapStateToProps)(ShopPage);
--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -3,7 +3,7 @@ import { createSelector } from 'reselect';
 const selectShop = state => state.shop;
 
 // memoize selector
-export const selectShopItems = createSelector(
+export const selectCollections = createSelector(
     [selectShop],
-    shop => shop.selectShopItems
+    shop => shop.collections
   );
