76dc724df8497d54e90a5fc08d4b51c70bbabe7d
Beta-23 <alfred.dominguez@gmail.com>
2020-01-21 22:50:30 -0800

    Implemented the styled-components library and refactor styles for the homepage component with css in js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { createStructuredSelector } from 'reselect';
 import { connect } from 'react-redux';
