5d55719ce3effa5b60fbaffe3c5fae573ccc114c
Beta-23 <alfred.dominguez@gmail.com>
2020-01-25 22:39:46 -0800

    Update UI to the cart-dropdown component

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -22,7 +22,7 @@ const CartDropdown = ({ cartItems, history, dispatch }) => (
           <CartItem key={cartItem.id} item={cartItem} />
         ))
       ) : (
-        <EmptyMessageContainer>Your cart is empty</EmptyMessageContainer>
+        <EmptyMessageContainer>Your Cart Is Empty!</EmptyMessageContainer>
       )}
     </CartItemsContainer>
     <CartDropdownButton
@@ -31,7 +31,7 @@ const CartDropdown = ({ cartItems, history, dispatch }) => (
         dispatch(toggleCartHidden());
       }}
     >
-      GO TO CHECKOUT
+      CHECKOUT NOW
     </CartDropdownButton>
   </CartDropdownContainer>
 );
