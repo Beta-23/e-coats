1b3e4a33ec54434934ff850708b531925ddd222f
Beta-23 <alfred.dominguez@gmail.com>
2020-02-28 14:47:12 -0800

    Fix typo in the shop saga and create the root saga to handle async actions

--- src/redux/store.js
+++ src/redux/store.js
@@ -9,9 +9,9 @@ import logger from 'redux-logger';
 // Middlewares Saga for redux store
 import createSagaMiddleware from 'redux-saga';
 
-import { fetchCollectionsStart } from './shop/shop.sagas';
-
 import rootReducer from './root-reducer';
+
+import rootSaga from './root-saga';
 
 const sagaMiddleware = createSagaMiddleware();
 
@@ -23,6 +23,8 @@ if (process.env.NODE_ENV === 'development') {
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-sagaMiddleware.run(fetchCollectionsStart);
+sagaMiddleware.run(rootSaga);
 
 export const persistor = persistStore(store);
+
+export default { store, persistStore };
